617204f262acb7c838186b62396a542ef27cf77d
Solid <solid@solids-macbook-air.local>
2018-12-14 13:12:33 +0300

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Solid on 12/14/18.
+//  Copyright © 2018 Solid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
